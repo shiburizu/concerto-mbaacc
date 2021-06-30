b9b01ab1e2358378ab07b19b59ec0672730d3a0f
Kevin Higgins <shiburizu312@gmail.com>
2021-06-29 22:38:46 -0600

    ship Roboto in res to avoid searching user dirs

--- config.py
+++ config.py
@@ -1,21 +1,33 @@
 from kivy.config import Config
 from kivy.lang import Builder
+Builder.load_file('ConcertoLobby.kv')
 import configparser
 import os
 import sys
-from kivy.resources import resource_add_path
+from kivy.resources import resource_add_path, resource_find
 if hasattr(sys, '_MEIPASS'):
     resource_add_path(os.path.join(sys._MEIPASS))
 os.environ["KIVY_AUDIO"] = "sdl2"
 print(os.path.dirname(sys.executable))
-Builder.load_file("ConcertoLobby.kv")  # .kv defines UI
 # Kivy app configs
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '400')
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'multisamples', 0)
+Config.set('kivy', 'desktop', 1)
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('kivy', 'window_icon', 'concertoicon.ico')
+Config.set(
+    "kivy",
+    "default_font",
+    [
+        "Roboto",
+        'data/fonts/Roboto-Regular.ttf', 
+        'data/fonts/Roboto-Italic.ttf',
+        'data/fonts/Roboto-Bold.ttf', 
+        'data/fonts/Roboto-BoldItalic.ttf'
+    ],
+)
 Config.write()
 
 # load caster .ini
