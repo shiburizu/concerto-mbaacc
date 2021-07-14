54b780d87a8b23006f334df5d53e5163b5f11526
Jen Stehlik <dev@stehlik.me>
2021-07-14 09:53:24 +0200

    very wip idea

--- presence.py
+++ presence.py
@@ -5,7 +5,7 @@ from pypresence import Presence
 APP_ID = '864412248310284289'
 RPC = Presence(APP_ID)
 
-previous = ['menu']
+previous = ['Menu']
 
 def connect():
     RPC.connect()
@@ -15,13 +15,18 @@ def close():
 
 # switch to previous presence status
 def to_previous():
-    if previous[0] == 'menu':
+    if previous[0].lower() == 'menu':
         menu()
-    elif previous[0] == 'public':
+    elif previous[0].lower() == 'public lobby':
         public_lobby(previous[1])
-    elif previous[0] == 'private':
+    elif previous[0].lower() == 'private lobby':
         private_lobby()
 
+def in_game():
+    if previous[0].lower() == 'public lobby':
+        public_online_game(previous[1], previous[2])
+    else
+        
 def menu():
     generic('Menu')
 
@@ -30,18 +35,21 @@ def character_select():
 
 def generic(mode):
     RPC.update(start=int(time.time()), details=mode, large_image='concerto_icon')
-    previous = [mode.lower()]
+    previous = [mode]
 
-def public_lobby(id):
-    RPC.update(state='Idle', start=int(time.time()), details='In Public Lobby', large_image='concerto_icon', buttons=[{'label': 'Join', 'url': 'concerto://lobby:' + str(id)}])
-    previous = ['public', id]
+def public_lobby(id, opponent_name):
+    RPC.update(state='Idle', start=int(time.time()), details='Public Lobby', large_image='concerto_icon', buttons=[{'label': 'Join', 'url': 'concerto://lobby:' + str(id)}])
+    previous = ['Public Lobby', id, opponent_name]
 
-def private_lobby():
-    RPC.update(state='Idle', start=int(time.time()), details='In Private Lobby', large_image='concerto_icon')
-    previous = ['private']
+def private_lobby(opponent_name):
+    RPC.update(state='Idle', start=int(time.time()), details='Private Lobby', large_image='concerto_icon')
+    previous = ['Private Lobby', opponent_name]
 
-def online_game(opponent_name, char_name, char_id, moon_id):
-    RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details='Online Game', large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id))
+def online_game(mode, opponent_name, char_name, char_id, moon_id):
+    RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details=mode, large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id))
+    
+def public_online_game(id, opponent_name, char_name, char_id, moon_id):
+    RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details='Public Lobby', large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id), buttons=[{'label': 'Join', 'url': 'concerto://lobby:' + str(id)}])
 
 def offline_game(gamemode, char_name, char_id, moon_id):
     RPC.update(start=int(time.time()), details=gamemode, large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id))
