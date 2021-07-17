d9dad64606620274bbe70f164308ed459f69410e
Kevin Higgins <shiburizu312@gmail.com>
2021-07-17 00:28:16 -0600

    bump version for hotfix

--- config.py
+++ config.py
@@ -74,4 +74,4 @@ app_config.read_string(config_string)
 
 LOBBYURL = "https://concerto-mbaacc.herokuapp.com/l"
 VERSIONURL = "https://concerto-mbaacc.herokuapp.com/v"
-CURRENT_VERSION = '7-16-2021'
+CURRENT_VERSION = '7-16-2021r2'
