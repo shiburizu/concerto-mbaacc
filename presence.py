ce7835495c6da279b0ca213c23caa8b4c89ce2f5
Kevin Higgins <shiburizu312@gmail.com>
2021-07-19 03:17:07 -0600

    fix logging in presence

--- presence.py
+++ presence.py
@@ -12,21 +12,21 @@ def connect():
         RPC.connect()
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def close():
     try:
         RPC.close()
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def menu():
     try:
         RPC.update(start=int(time.time()), details='Menu', large_image='concerto_icon')
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def character_select(mode,lobby_id=None):
     try:
@@ -36,7 +36,7 @@ def character_select(mode,lobby_id=None):
             RPC.update(state='Character Select', start=int(time.time()), details=mode, large_image='concerto_icon')
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def generic(mode):
     try:
@@ -49,28 +49,28 @@ def public_lobby(id):
         RPC.update(state='Idle', start=int(time.time()), details='Public Lobby #%s' % id, large_image='concerto_icon', buttons=[{'label': 'Join Lobby', 'url': 'concerto://lobby:' + str(id)}])
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def private_lobby():
     try:
         RPC.update(state='Idle', start=int(time.time()), details='Private Lobby', large_image='concerto_icon')
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def online_game(mode, opponent_name, char_name, char_id, moon_id):
     try:
         RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details=mode, large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id))
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def public_lobby_game(lobby_id, opponent_name, char_name, char_id, moon_id):
     try:
         RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details='Public Lobby #%s' % lobby_id, large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id), buttons=[{'label': 'Join Lobby', 'url': 'concerto://lobby:' + str(lobby_id)}])
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def broadcast_game(mode, char1_id, tooltip_1, char2_id, tooltip_2, lobby_id=None):
     try:
@@ -80,7 +80,7 @@ def broadcast_game(mode, char1_id, tooltip_1, char2_id, tooltip_2, lobby_id=None
             RPC.update(start=int(time.time()), state=mode, large_image='char_' + str(char1_id), large_text=tooltip_1, small_image='char_' + str(char2_id), small_text=tooltip_2)
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def offline_game(gamemode, char1_name, char1_id, char2_name, char2_id, lobby_id=None):
     try:
@@ -90,7 +90,7 @@ def offline_game(gamemode, char1_name, char1_id, char2_name, char2_id, lobby_id=
             RPC.update(start=int(time.time()), state=gamemode, large_image='char_' + str(char1_id), large_text=char1_name, small_image='char_' + str(char2_id), small_text=char2_name)
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
 
 def single_game(mode,char_name, char_id, moon_id, lobby_id=None):
     try:
@@ -100,4 +100,4 @@ def single_game(mode,char_name, char_id, moon_id, lobby_id=None):
             RPC.update(start=int(time.time()), state=mode, large_image='char_' + str(char_id), large_text=char_name, small_image='moon_' + str(moon_id))
     except:
         s = ''.join(traceback.format_stack())
-        logging.log(s)
+        logging.warning(s)
