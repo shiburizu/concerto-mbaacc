bb955056d77d5f9d0d6af6a7d19b28f2e17abdcd
Kevin Higgins <shiburizu312@gmail.com>
2021-07-21 00:34:06 -0600

    excepts

--- presence.py
+++ presence.py
@@ -52,10 +52,16 @@ def private_lobby():
         logging.warning('Concerto: DISCORD: %s' % sys.exc_info()[0])
 
 def online_game(mode, opponent_name, char1_name, char1_id, char2_name, char2_id):
-    RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details=mode, large_image='char_' + str(char1_id), large_text=char1_name, small_image='char_' + str(char2_id), small_text=char2_name)
+    try:
+        RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details=mode, large_image='char_' + str(char1_id), large_text=char1_name, small_image='char_' + str(char2_id), small_text=char2_name)
+    except:
+        logging.warning('Concerto: DISCORD: %s' % sys.exc_info()[0])
 
 def public_lobby_game(lobby_id, opponent_name, char1_name, char1_id, char2_name, char2_id):
-    RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details='Public Lobby #%s' % lobby_id, large_image='char_' + str(char1_id), large_text=char1_name, small_image='char_' + str(char2_id), small_text='char_' + str(char2_name), buttons=[{'label': 'Join Lobby', 'url': 'concerto://lobby:' + str(lobby_id)}])
+    try:
+        RPC.update(state='Playing vs ' + opponent_name, start=int(time.time()), details='Public Lobby #%s' % lobby_id, large_image='char_' + str(char1_id), large_text=char1_name, small_image='char_' + str(char2_id), small_text='char_' + str(char2_name), buttons=[{'label': 'Join Lobby', 'url': 'concerto://lobby:' + str(lobby_id)}])
+    except:
+        logging.warning('Concerto: DISCORD: %s' % sys.exc_info()[0])
 
 def broadcast_game(mode, char1_id, tooltip_1, char2_id, tooltip_2, lobby_id=None):
     try:
