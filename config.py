7068ec44c483e7537015c846bfdbf32ba3636e6c
Kevin Higgins <shiburizu312@gmail.com>
2021-08-01 14:57:52 -0600

    bump client version

--- config.py
+++ config.py
@@ -115,4 +115,4 @@ app_config.read_string(config_string)
 
 LOBBYURL = "https://concerto-mbaacc.herokuapp.com/l"
 VERSIONURL = "https://concerto-mbaacc.herokuapp.com/v"
-CURRENT_VERSION = '8-1-2021'
+CURRENT_VERSION = '8-1-2021r2'
